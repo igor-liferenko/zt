f4c72ab78844b39b471a110a8bf11ef993df4334
Igor Liferenko <igor.liferenko@gmail.com>
2019-12-23 12:28:41 +0700

    fix

--- src/include/private/ftdm_types.h
+++ src/include/private/ftdm_types.h
@@ -204,6 +204,10 @@ typedef enum {
 	FTDM_CHANNEL_FEATURE_MF_GENERATE = (1<<10), /*!< Channel can generate R2 MF tones (read-only) */
 } ftdm_channel_feature_t;
 
+#define FTDM_CHANNEL_IO_EVENT (1 << 0)
+#define FTDM_CHANNEL_IO_READ (1 << 1)
+#define FTDM_CHANNEL_IO_WRITE (1 << 2)
+
 /*!< Channel flags. This used to be an enum but we reached the 32bit limit for enums, is safer this way */
 #define FTDM_CHANNEL_CONFIGURED    (1ULL << 0)
 #define FTDM_CHANNEL_READY         (1ULL << 1)
